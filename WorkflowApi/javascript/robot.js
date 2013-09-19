ce84cad5f09d47e44d6da37cc4f2c35817411e5b
brad-fitzwater <brad.fitzwater@rackspace.com>
2013-09-19 14:46:32 -0400

    garbage.

--- WorkflowApi/javascript/robot.js
+++ WorkflowApi/javascript/robot.js
@@ -3,8 +3,10 @@ function log(text) {
     $("#log").append(".\n" + text);
 }
 
+var speed = 200;
 // The next URL to retrieve
 var next = undefined;
+var queue = [];
 
 // Follow the "next" document, add them to the queue, and "tock()"
 function tick() {
@@ -16,57 +18,37 @@ function tick() {
     $.getJSON(next, function (data) {
         var currentPage = next;
         console.log("got data", data);
-        if (data.collection && data.collection.items) {
-            if (data.collection.items.length == 0) {
-                log("Empty collection, retrying again later");
-                setTimeout(tick, 10 * speed);
-                return;
-            }
-            next = undefined;
-            log("Found " + data.collection.items.length + " items");
-            // assume it's a collection json; since this is a
-            // demo it uses introspection instead of media types
-            queue = queue.concat(data.collection.items);
-
-            if (typeof data.collection.links == 'array') {
-                $.each(data.collection.links, function (key, val) {
-                    if (val.rel == "next") next = 'http://10.0.12.137:1234' + val.href;
-                    console.log("next", next);
-                });
-            }
-            else {
-                console.log("no next link, reloading in a bit.");
-                setTimeout(function () {
-                    next = currentPage;
-                    tick();
-                }, 10 * speed);
-            }
-        }
+        queue = queue.concat(data.collection.items);
         tock();
     });
 }
 
 // figure out what to do...
 function tock() {
+    console.log("tock");
     if (queue.length == 0) {
         tick();
         return;
     }
     var task = queue.shift();
-    log("Following " + 'http://10.0.12.137:1234' + task.href);
+    console.log("task");
+    console.log(task);
+    console.log(task.href);
+    console.log("Following " + 'http://10.0.12.137:1234' + task.href);
     $.getJSON('http://10.0.12.137:1234' + task.href, function (data) {
-        log("Got Data: " + data.type);
+        console.log("Got Data: " + data.type);
         if (data.type != "http://mogsie.com/2013/workflow/ReadToMe") {
             tock();
             return;
         }
         // take job if it says so
-        log("Data.start: " + data.start);
+        console.log("Data.start: " + data.start);
         if (data.start) {
-
-            console.log("I should really start work before doing this..");
+            console.log(data);
             var player = new window.GoogleTTS()
+            console.log(data.input.text);
             player.play(data.input.text, "en", function (err) {
+                console.log(err);
                 console.log('Finished playing');
             });
         }
