d1fbb1488f856f4987e3181244c5babbfa441ad0
James Morgan <j.a.mrgn@gmail.com>
2013-09-19 10:11:33 -0400

    Added the shell solution

---
+++ WorkflowApi/Controllers/WorkflowController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkflowApi.Controllers
+{
+    public class WorkflowController : Controller
+    {
+        //
+        // GET: /Workflow/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WorkflowApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkflowApi.WebApiApplication" Language="C#" %>
