70843ed649c40bb74d0f2194992c7b4d13f10e77
brad-fitzwater <brad.fitzwater@rackspace.com>
2013-09-19 10:20:53 -0400

    initial attempt at text to speech.

---
+++ WorkflowClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer s = new SpeechSynthesizer();
+            s.Speak("Hello. World.");
+        }
+    }
+}
